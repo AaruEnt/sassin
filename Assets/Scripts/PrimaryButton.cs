20fe48d80c9a66d35ad2f590af00c12d19cd7483
Brett Davis <brett.p.davis101@gmail.com>
2022-08-26 15:00:24 -0500

    Sliding

--- Assets/Scripts/PrimaryButton.cs
+++ Assets/Scripts/PrimaryButton.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Autohand;
+using NaughtyAttributes;
 
 public class PrimaryButton : MonoBehaviour
 {
-    // if false, defaults to slide
-    public bool jumpOnPress;
     public Rigidbody rb;
     public AutoHandPlayer player;
     public MomentumController momentum;
+
+    // if false, defaults to slide
+    public bool jumpOnPress;
+    [ShowIf("jumpOnPress")]
     public float jumpHeight = 40;
+    [ShowIf("jumpOnPress")]
     public float maxJumpHeight = 50;
+    [HideIf("jumpOnPress")]
+    public float slideForce = 10f;
+
+    private bool isSliding = false;
+
+    void Update() {
+        //if (!isSliding && player.IsCrouching())
+        //    Slide();
+    }
 
     public void OnPrimaryButton() {
         if (jumpOnPress) {
@@ -30,6 +43,20 @@ public class PrimaryButton : MonoBehaviour
     }
 
     private void Slide() {
-        Debug.Log("Slide");
+        if (!isSliding && momentum.counter >= 800) {
+            isSliding = true;
+            StartCoroutine(DoSlide());
+        }
+    }
+
+    private IEnumerator DoSlide() {
+        player.crouching = true;
+        float tmp = momentum.maxSpeedScale;
+        momentum.maxSpeedScale = tmp + 2;
+        rb.AddRelativeForce(Vector3.forward * slideForce, ForceMode.Impulse);
+        yield return new WaitForSeconds(1);
+        isSliding = false;
+        player.crouching = false;
+        momentum.maxSpeedScale = tmp;
     }
 }
