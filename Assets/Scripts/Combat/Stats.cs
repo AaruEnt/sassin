892ae6d00c2a01d61f3f1409c687bf9f82f3d39c
Brett Davis <brett.p.davis101@gmail.com>
2022-10-31 22:10:02 -0500

    Combat documentation

--- Assets/Scripts/Combat/Stats.cs
+++ Assets/Scripts/Combat/Stats.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Stats : MonoBehaviour
 {
-    // health
-    public float health = 20f;
-    // maximum health
-    public float maxHealth = 20f;
-    // unarmed damage
-    public float damage = 2f;
+    [Header("Variables")]
+    [SerializeField, Tooltip("health")]
+    internal float health = 20f;
 
+    [SerializeField, Tooltip("maximum health")]
+    internal float maxHealth = 20f;
+
+    [SerializeField, Tooltip("unarmed damage")]
+    internal float damage = 2f;
+
+    // When damage is received
     void OnDamageReceived(float damage) {
         if (damage > 0)
             health -= damage;
@@ -18,6 +22,7 @@ public class Stats : MonoBehaviour
             OnKill();
     }
 
+    // When damage is healed
     void OnHeal(float healAmount) {
         if (healAmount > 0)
             health += healAmount;
@@ -25,10 +30,12 @@ public class Stats : MonoBehaviour
             health = maxHealth;
     }
 
+    // When health reaches 0 or less
     void OnKill() {
         Destroy(this.gameObject);
     }
 
+    // When collision is detected, check if it affects health
     void OnCollisionEnter(Collision col) {
         if (col.gameObject.tag == "Effect") { // Includes spells and weapons that deal damage, heal, or create some form of effect
             Collider hitCol = col.contacts[0].thisCollider;
