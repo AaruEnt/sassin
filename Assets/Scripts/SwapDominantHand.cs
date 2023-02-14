047e7603f7411fc1e9810c698cdc472e75bb097a
Brett Davis <brett.p.davis101@gmail.com>
2023-02-14 14:26:54 -0600

    Hand swapping functional

--- Assets/Scripts/SwapDominantHand.cs
+++ Assets/Scripts/SwapDominantHand.cs
@@ -1,23 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Autohand;
 
+// Controlls collision between hands and watches, as well as the visibility of models attached to watch
 public class SwapDominantHand : MonoBehaviour
 {
     public bool WatchLeftHand = true; // assumes watch on left hand
     public GameObject leftWatch;
     public GameObject rightWatch;
+    public GameObject leftHand;
+    public GameObject rightHand;
     public GameObject leftModels;
     public GameObject rightModels;
+    public Grabbable leftGrab;
+    public Grabbable rightGrab;
+
+    private Collider[] leftHandColliders;
+    private Collider[] rightHandColliders;
+    private Collider[] leftWatchColliders;
+    private Collider[] rightWatchColliders;
 
     // Start is called before the first frame update
     void Start()
     {
+        leftHandColliders = leftHand.GetComponentsInChildren<Collider>();
+        rightHandColliders = rightHand.GetComponentsInChildren<Collider>();
+        leftWatchColliders = leftWatch.GetComponentsInChildren<Collider>();
+        rightWatchColliders = rightWatch.GetComponentsInChildren<Collider>();
+
+        foreach (Collider c in leftHandColliders)
+        {
+            foreach (Collider cl in leftWatchColliders)
+            {
+                Physics.IgnoreCollision(c, cl);
+            }
+        }
+        foreach (Collider c in rightHandColliders)
+        {
+            foreach (Collider cl in rightWatchColliders)
+            {
+                Physics.IgnoreCollision(c, cl);
+            }
+        }
         if (WatchLeftHand)
         {
             //EnableAllChildren(leftWatch);
             //DisableAllChildren(rightWatch);
-            DisableAllModels(rightModels);
+            SwapToLeft();
+        } else
+        {
+            SwapToRight();
         }
     }
 
@@ -36,6 +69,10 @@ public class SwapDominantHand : MonoBehaviour
         //DisableAllChildren(rightWatch);
         EnableAllModels(leftModels);
         DisableAllModels(rightModels);
+        rightGrab.enabled = false;
+        leftGrab.enabled = true;
+        StartCoroutine(EnableCollision(leftWatchColliders, rightHandColliders));
+        StartCoroutine(DisableCollision(rightWatchColliders, leftHandColliders));
     }
 
     void SwapToRight()
@@ -45,24 +82,10 @@ public class SwapDominantHand : MonoBehaviour
         //DisableAllChildren(leftWatch);
         DisableAllModels(leftModels);
         EnableAllModels(rightModels);
-    }
-
-    void DisableAllChildren(GameObject obj)
-    {
-        foreach (Transform t in obj.transform)
-        {
-            if (t != obj.transform)
-                t.gameObject.SetActive(false);
-        }
-    }
-
-    void EnableAllChildren(GameObject obj)
-    {
-        foreach (Transform t in obj.transform)
-        {
-            if (t != obj.transform)
-                t.gameObject.SetActive(true);
-        }
+        rightGrab.enabled = true;
+        leftGrab.enabled = false;
+        StartCoroutine(EnableCollision(rightWatchColliders, leftHandColliders));
+        StartCoroutine(DisableCollision(leftWatchColliders, rightHandColliders));
     }
 
     void DisableAllModels(GameObject obj)
@@ -81,5 +104,29 @@ public class SwapDominantHand : MonoBehaviour
         {
             m.enabled = true;
         }
+    }
+
+    private IEnumerator DisableCollision(Collider[] watch, Collider[] hand)
+    {
+        foreach (Collider c in watch)
+        {
+            foreach (Collider cl in hand)
+            {
+                Physics.IgnoreCollision(c, cl);
+            }
+        }
+        yield return null;
+    }
+
+    private IEnumerator EnableCollision(Collider[] watch, Collider[] hand)
+    {
+        foreach (Collider c in watch)
+        {
+            foreach (Collider cl in hand)
+            {
+                Physics.IgnoreCollision(c, cl, false);
+            }
+        }
+        yield return null;
     }
 }
---
+++ Assets/Scripts/UnparentOnStart.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnparentOnStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
