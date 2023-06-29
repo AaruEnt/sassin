b7c7e7fe8eb5e953508e8a8b8bdaae006f3b8905
Brett Davis <brett.p.davis101@gmail.com>
2023-06-29 00:44:16 -0500

    corrected hand rotation

--- Assets/Scripts/Multiplayer/NetworkPlayer.cs
+++ Assets/Scripts/Multiplayer/NetworkPlayer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using Photon.Pun;
+using System.Diagnostics;
 
 public class NetworkPlayer : MonoBehaviourPunCallbacks
 {
@@ -10,6 +11,12 @@ public class NetworkPlayer : MonoBehaviourPunCallbacks
     public Transform left;
     public Transform right;
     public static GameObject player;
+    public Vector3 leftRotOffset;
+    public Vector3 rightRotOffset;
+
+    internal GameObject leftHand;
+    internal GameObject rightHand;
+    internal GameObject headCamera;
 
     // Start is called before the first frame update
     void Start()
@@ -27,19 +34,26 @@ public class NetworkPlayer : MonoBehaviourPunCallbacks
             right.gameObject.SetActive(false);
             left.gameObject.SetActive(false);
             head.gameObject.SetActive(false);
-            MapPosition(head, XRNode.Head);
-            MapPosition(left, XRNode.LeftHand);
-            MapPosition(right, XRNode.RightHand);
+            MapPosition(head, XRNode.Head, Quaternion.identity.eulerAngles);
+            MapPosition(left, leftHand, leftRotOffset);
+            MapPosition(right, rightHand, rightRotOffset);
         }
     }
 
-    void MapPosition(Transform target, XRNode node)
+    void MapPosition(Transform target, XRNode node, Vector3 rotOffset)
     {
         InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
         InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
 
         target.localPosition = position;
-        target.rotation = rotation;
+        target.rotation = rotation * Quaternion.Euler(rotOffset);
+    }
+
+    void MapPosition(Transform target, GameObject node, Vector3 rotOffset = default(Vector3))
+    {
+        UnityEngine.Debug.Log("In override");
+        target.localPosition = node.transform.localPosition;
+        target.rotation = node.transform.rotation;
     }
 
     internal void UpdatePositionRotation(Transform position, Quaternion rotation)
--- Assets/Scripts/Multiplayer/NetworkPositionLink.cs
+++ Assets/Scripts/Multiplayer/NetworkPositionLink.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class NetworkPositionLink : MonoBehaviour
 {
+    public GameObject head;
+    public GameObject left;
+    public GameObject right;
+
     private GameObject networkPlayer;
     private NetworkPlayer player;
     // Start is called before the first frame update
@@ -14,6 +18,9 @@ public class NetworkPositionLink : MonoBehaviour
         {
             networkPlayer = NetworkPlayer.player;
             player = networkPlayer.GetComponent<NetworkPlayer>();
+            player.rightHand = right;
+            player.leftHand = left;
+            player.headCamera = head;
         }
     }
 
@@ -22,15 +29,18 @@ public class NetworkPositionLink : MonoBehaviour
     {
         if (networkPlayer != null && player != null)
         {
-            UnityEngine.Debug.Log(string.Format("pos: {0}, rot: {1}", this.transform.position, this.transform.rotation));
+            //UnityEngine.Debug.Log(string.Format("pos: {0}, rot: {1}", this.transform.position, this.transform.rotation));
             player.UpdatePositionRotation(this.transform, this.transform.rotation);
         } else
         {
-            UnityEngine.Debug.Log("Looking for self");
+            //UnityEngine.Debug.Log("Looking for self");
             if (NetworkPlayer.player)
             {
                 networkPlayer = NetworkPlayer.player;
                 player = networkPlayer.GetComponent<NetworkPlayer>();
+                player.rightHand = right;
+                player.leftHand = left;
+                player.headCamera = head;
             }
         }
     }
