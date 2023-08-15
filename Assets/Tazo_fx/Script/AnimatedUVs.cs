56b47bc02dde80418c5def5396224aad14b6e084
dragonslumberinc <126087978+dragonslumberinc@users.noreply.github.com>
2023-08-15 15:06:37 -0400

    Added realistic waterfall asset
    Added realistic waterfall asset

---
+++ Assets/Tazo_fx/Script/AnimatedUVs.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatedUVs : MonoBehaviour {
+		public float speedY = 0.5F;
+		public float speedx = 0.0F;
+		private float offsety = 0.0F;
+		private float offsetx = 0.0F;
+		private Renderer rend;
+		void Start() {
+				rend = GetComponent<Renderer>();
+		}
+		void Update() {
+				offsety += Time.deltaTime * speedY;
+				offsetx += Time.deltaTime * speedx;
+				rend.material.SetTextureOffset("_MainTex", new Vector2(offsetx,offsety));
+		}
+}
---
+++ Assets/Tazo_fx/Script/tazo_UVScroller_r.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class tazo_UVScroller_r : MonoBehaviour {
+
+public int targetMaterialSlot=0;
+		private Renderer myrender;
+public float speedY=0.5f;
+public float speedX=0.0f;
+private float timeWentX=0;
+private float timeWentY=0;
+
+void Start (){
+				
+		myrender = GetComponent<Renderer>();
+}
+
+void Update (){
+		timeWentY += Time.deltaTime*speedY;
+		timeWentX += Time.deltaTime*speedX;
+		myrender.material.SetTextureOffset("_DistortTex",new Vector2(timeWentX, timeWentY));
+		myrender.material.SetTextureOffset("_RefractionLayer",new Vector2(timeWentX, timeWentY));
+}
+
+//void OnEnable (){
+//
+//		myrender.material.SetTextureOffset ("_MainTex",new Vector2(0, 0));
+//		timeWentX = 0;
+//		timeWentY = 0;
+//}
+}
