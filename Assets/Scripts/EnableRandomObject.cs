d118af8553a5ca91bcd07e4c0f51a13e0e502d5a
Brett Davis <brett.p.davis101@gmail.com>
2023-08-29 19:13:59 -0500

    Network player has a gourd head and a chance to have a random hat and/or necklace

--- Assets/Scripts/EnableRandomObject.cs
+++ Assets/Scripts/EnableRandomObject.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class EnableRandomObject : MonoBehaviour
 {
     public List<GameObject> pickFrom = new List<GameObject> ();
+    public float probability = 100f;
 
     // Start is called before the first frame update
     void Start()
     {
-        var tmp = Randomizer.PickRandomObject(pickFrom);
+        if (Randomizer.Prob(probability))
+        {
+            var tmp = Randomizer.PickRandomObject(pickFrom);
 
-        if (tmp) {
-            tmp.SetActive(true);
+            if (tmp)
+            {
+                tmp.SetActive(true);
+            }
         }
     }
 }
