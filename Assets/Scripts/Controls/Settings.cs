c877284dd05ada123b1f6855e92e018d1f94c75c
Brett Davis <brett.p.davis101@gmail.com>
2024-04-10 21:51:20 -0500

    started on basic settings stuff

---
+++ Assets/Scripts/Controls/Settings.cs
@@ -0,0 +1,24 @@
+using Autohand;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using Autohand;
+
+public class Settings : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public AutoHandPlayer player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
