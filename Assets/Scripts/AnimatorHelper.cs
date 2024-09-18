6d61cdfe774f3fe2307f6e17840f4ba9d8a5d9ad
AaruEnt <91345184+AaruEnt@users.noreply.github.com>
2024-09-18 18:36:48 -0500

    meeting shit

--- Assets/Scripts/AnimatorHelper.cs
+++ Assets/Scripts/AnimatorHelper.cs
@@ -8,6 +8,22 @@ public class AnimatorHelper : MonoBehaviour
 {
     public Animator anim;
     public AudioSource audio;
+    public bool setRandomDelayOnStart = false;
+    public string defaultParam = "";
+    private Vector3 _startPos;
+    private Quaternion _startRot;
+    public Vector3 pos;
+    public Quaternion rot;
+
+    private void Start()
+    {
+        if (setRandomDelayOnStart)
+        {
+            SetRandomValOnStart(defaultParam);
+        }
+        _startPos = transform.position;
+        _startRot = transform.rotation;
+    }
 
     public void PlayAnim(string animParam)
     {
@@ -25,5 +41,32 @@ public class AnimatorHelper : MonoBehaviour
     public void PlaySound()
     {
         audio.Play();
+    }
+
+    public void SetRandomValOnStart(string param)
+    {
+        StartCoroutine(RandomDelay(param));
+    }
+
+    private IEnumerator RandomDelay(string param)
+    {
+        yield return new WaitForSeconds((float)Randomizer.RandomRange(3, 0));
+        anim.SetBool(param, true);
+    }
+
+    public void MoveToPos()
+    {
+        anim.enabled = false;
+        transform.position = pos;
+        transform.rotation = rot;
+        anim.enabled = true;
+    }
+
+    public void ResetPos()
+    {
+        anim.enabled = false;
+        transform.position = _startPos;
+        transform.rotation = _startRot;
+        anim.enabled = true;
     }
 }
